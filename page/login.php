eeb979e2b3bc4d29538e94afbf15a92664456172
Martin Viidik <martvii@greeny.cs.tlu.ee>
2015-09-16 13:33:16 +0300

    added features

--- page/login.php
+++ page/login.php
@@ -9,48 +9,91 @@
 	$reg_password_error = "";
 	$reg_username_error = "";
 	
+	//muutujad andmebaasi väärtuste jaoks
+	$username = ""; $email = ""; $password = "";
+	
 	//echo $_POST["email"];
 	
 	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
-		//echo "keegi vajutas nuppu";
-		if (empty($_POST["email"]) ) {
-			$email_error = "This field is required";
-			}
-			
-		if (empty($_POST["username"]) ) {
-			$username_error = "This field is required";
-			}
-			
-		if (empty($_POST["password"]) ) {
-			$password_error = "This field is required";
-			}
-			
-		if (empty($_POST["reg_username"]) ) {
-			$reg_username_error = "This field is required";
-			}
-		if (empty($_POST["reg_password"]) ) {
-			$reg_password_error = "This field is required";
-			}
-		if (empty($_POST["reg_email"]) ) {
-			$reg_email_error = "This field is required";
-			}
+		//************************************************************
+		//echo "keegi vajutas login nuppu";
 		
-		// kontrollin, et parool ei ole tühi
-		if (empty($_POST["password"]) ) {
-			$password_error = "This field is required";
-			} else {
+		//vajutas login nuppu
+		//************************************************************
+		if(isset($_POST["login"])){
+				
+			if (empty($_POST["email"]) ) {
+				$email_error = "This field is required";
+			}else{
+				// kõik korras
+				// test_input eemaldab pahataltikud osad
+				$email = test_input($_POST["email"]);
+				
+				}
+				
+			if (empty($_POST["username"]) ) {
+				$username_error = "This field is required";
+				
+			}else{
+				
+				$username = test_input($_POST["username"]);
+				
+				}
+				
+			if (empty($_POST["password"]) ) {
+				$password_error = "This field is required";
+			}else{
+			
+				$password = test_input($_POST["password"]);
+				
+			}
+			// kontrollin et ei oleks ühtegi errorit
+			if($email_error == "" && $password_error ==""){
+				
+			echo "kontrollin sisselogimist ".$email." ja parool ";
+		
+			}
+
+		
+		//*************************************************************
+		// Keegi vajutas create nuppu
+		//*************************************************************
+		}elseif(isset($_POST["create"])){
+			
+			if (empty($_POST["reg_username"]) ) {
+				$reg_username_error = "This field is required";
+			}
+			if (empty($_POST["reg_password"]) ) {
+				$reg_password_error = "This field is required";
+			}else {
 				
 				// kui oleme siia jõudnud, siis parool ei ole tühi enam
 				// kontrollin, et oleks vähemalt 8 sümbolit pikk
-				if(strlen($_POST["password"]) < 8) {
+				if(strlen($_POST["reg_password"]) < 8) {
 					
-					$password_error = "Password must be at least 8 characters";
+					$reg_password_error = "Password must be at least 8 characters";
 					
+				}
 			}
+			if (empty($_POST["reg_email"]) ) {
+				$reg_email_error = "This field is required";
+			}
+		
 		}
 	}
+	
+	
+	function test_input($data) {
+		// võtab ära tühikud, enterid, tabid
+		$data = trim($data);
+		// tagurpidi kaldkriipsud
+		$data = stripslashes($data);
+		// teeb htm'li tekstiks 
+		$data = htmlspecialchars($data);
+		return $data;
+}
 	
 	$page_title = "Sisselogimine";
 	$page_file_name = "login.php";
@@ -63,17 +106,17 @@
 			<input name="username" type="text" placeholder="username"> <?php echo $username_error; ?><br> <br>
 			<input name="email" type="email" placeholder="email"> <?php echo $email_error; ?><br> <br>
 			<input name="password" type="password" placeholder="Password"> <?php echo $password_error; ?> <br> <br>
-			<input type="submit" value="Log in"> <br> <br>
+			<input name="login" type="submit" value="Log in"> <br> <br>
 		</form>
 	
 	<h2>Create user</h2>
 	
 		<form action="login.php" method="post" >
-			<input name="reg_username" type="text" placeholder="username"> <?php echo $reg_username_error; ?><br> <br>
-			<input name="reg_email" type="email" placeholder="email"> <?php echo $reg_email_error; ?><br> <br>
-			<input name="reg_password" type="password" placeholder="Password"> <?php echo $reg_password_error; ?> <br> <br>
-			<input name="reg_password" type="password" placeholder="Insert password again"> <?php echo $reg_password_error; ?> <br> <br>
-			<input type="submit" value="Register"> <br> <br>
+			<input name="reg_username" type="text" placeholder="username"> * <?php echo $reg_username_error; ?><br> <br>
+			<input name="reg_email" type="email" placeholder="email"> * <?php echo $reg_email_error; ?><br> <br>
+			<input name="reg_password" type="password" placeholder="Password"> * <?php echo $reg_password_error; ?> <br> <br>
+			<input name="reg_password" type="password" placeholder="Insert password again"> * <?php echo $reg_password_error; ?> <br> <br>
+			<input name="create" type="submit" value="Register"> <br> <br>
 		</form>
 	
 	<?php require_once("../footer.php"); ?>
