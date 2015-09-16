bd6785a5e00edb95a128e2b46494be5fb4d24580
Martin Viidik <martvii@greeny.cs.tlu.ee>
2015-09-16 11:23:04 +0300

    implemented footer and header

--- footer.php
+++ footer.php
@@ -1,1 +1,3 @@
-jalus
+	<p><i>Lehe tegi Martin, 2015</i></p>
+</body>
+</html>
--- page/login.php
+++ page/login.php
@@ -51,13 +51,10 @@
 			}
 		}
 	}
+	
+	require_once("../header.php");
 ?>
 
-<html>
-<head>
-	<title>Login page</title>
-</head>
-<body>
 	<h2>Log in</h2>
 		
 		<form action="login.php" method="post" >
@@ -77,6 +74,4 @@
 			<input type="submit" value="Register"> <br> <br>
 		</form>
 	
-	
-</body>
-</html>
+	<?php require_once("../footer.php"); ?>
